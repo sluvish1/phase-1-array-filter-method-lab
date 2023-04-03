31fa88031c7491dbfb5151238230f88ee91c7e7a
Vaschell Mkenzie <mvaschell@gmail.com>
2023-04-03 17:04:59 -0400

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+const allDrivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+//const findMatching = (names)=>nameOfDrivers.filter(name => nameOfDrivers);
+
+function findMatching(drivers,name){
+let results = drivers.filter(function(driver){
+    if(driver === name || driver === name.toLowerCase()){
+        return name
+    }
+}) 
+ return results}
+
+
+//console.log(typeof findMatching)
